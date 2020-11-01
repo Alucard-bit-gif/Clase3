1b019565e17e7636629bb478c49c3b1626c42cf9
Sebastián <serviciossociologicos@gmail.com>
2020-11-01 18:10:03 -0500

    Código para suma en javascript

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,1 +1,55 @@
+
+
+
+//Inicio//
+//Declarar variables//
+let N1;
+let N2;
+let Suma = 0;
+
+//Leer datos//
+N1 = Number(prompt('Ingrese el valor para Número 1'));
+N2 = Number(prompt('Ingrese el valor para Número2'));
+
+//Realizar proceso//
+
+Suma = N1 + N2;
+
+
+//Imprimir//
+console.log('La suma es:' + Suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*Este es el archivo dos creado en git*/
