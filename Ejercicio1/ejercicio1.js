871ae9a2e1c209f9e490dd990ddce239d417b6df
Sebastián <serviciossociologicos@gmail.com>
2020-11-01 23:39:04 -0500

    Se ingresó un h5 en html, en css  atributis y valores a h4 y en javascript el primer ejercicio resuelto

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,7 +1,6 @@
-
-
-
 //Inicio//
+
+
 //Declarar variables//
 let N1;
 let N2;
@@ -15,12 +14,31 @@ N2 = Number(prompt('Ingrese el valor para Número2'));*/
 
 Suma = N1 + N2;
 
+//---------------------------------------------------------------------------------------------------------->//
 
 //Imprimir//
 console.log('La suma es:' + Suma);
 
+//Problema 1 "Promedio de notas"//
+//Inicio//
+//Declarar Variables//
 
+let parcialUno = 3.5;
+let parcialDos = 4.0;
+let parcialTres = 4.5;
+let numNotas = 3;
+let totalParciales = (parcialUno + parcialDos + parcialTres)/numNotas;
 
+let exaFinal = 3.8;
+let proFinal = 4.2;
+
+let pesoParciales = 0.55;
+let pesoexaFinal = 0.30;
+let pesoproFinal = 0.15;
+
+let promedNotas = (totalParciales*pesoParciales) + (exaFinal*pesoexaFinal) + (proFinal*pesoproFinal);
+
+console.log("Promedio de notas en lógica y programación es :" + " " + promedNotas);
 
 
 
