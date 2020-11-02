eeb2aab431c52ac7b7d2fdbe15d42954acb8bad1
Sebastián <serviciossociologicos@gmail.com>
2020-11-02 02:45:41 -0500

    Se incluyó p para el problema en html; se aplico un selector para el articule en css y se realizó el ejercicio # 2 en js aplicando la extrapolación

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -36,17 +36,74 @@ let pesoParciales = 0.55;
 let pesoexaFinal = 0.30;
 let pesoproFinal = 0.15;
 
+//Realizar proceso
+
 let promedNotas = (totalParciales*pesoParciales) + (exaFinal*pesoexaFinal) + (proFinal*pesoproFinal);
 
+//Imprimir
 console.log("Promedio de notas en lógica y programación es :" + " " + promedNotas);
 
 document.querySelector(".rNumUno").innerHTML = "Respuesta";
 
 document.querySelector(".rUno").innerHTML = ("Promedio de notas en lógica y programación es :" + " " + promedNotas);
 
+//Final//
+
+//--------------------------------------------------------------------------------------------------------->//
+
+//Problema 1 "Promedio de notas"//
+//Inicio//
+//Declarar Variables//
+
+let inverUno = 'Carlos Mario';
+let inverDos = 'Hugo Alberto';
+let inverTres ='Edison Andrés';
+let pesoInver1 = 44414407;
+let pesoInver2 = 24226040;
+let pesoInver3 = 12113020;
+
+//Realizar proceso
+
+let inverTotal = `${pesoInver1 + pesoInver2 + pesoInver3}`; 
+let inverMayor = `${parseInt(pesoInver1/inverTotal*100)}`;
+let inverMedio = `${parseInt(pesoInver2/inverTotal*100)}`;
+let inverModerado = `${parseInt(pesoInver3/inverTotal*100)}`;
+
+let iM = `La mayor inversión es la hecha por ${inverUno},`;
+let iMQ = `$COP ${pesoInver1} pesos es la cantidad de su inversión;`;
+let iMQP = `equivalente al ${inverMayor}%.`;
+
+let iMdo = `La inversión media es la hecha por ${inverDos};`;
+let iMdoQ = `$COP ${pesoInver2} pesos es la cantidad de su inversión;`;
+let iMdoQP = `equivalente al ${inverMedio}%.`;
+
+let iMrdo = `La inversión moderada es la hecha por ${inverTres},`;
+let iMrdoQ = `$COP ${pesoInver3} pesos es la cantidad de su inversión;`;
+let iMrdoQP = `equivalente al ${inverModerado}%.`;
+
+let textToShowOne = `${iM} <br>
+                     ${iMQ} <br>
+                     ${iMQP}<br>`; 
+let textToShowTwo = `${iMdo} <br> 
+                     ${iMdoQ} <br>
+                     ${iMdoQP}<br>`;
+let textToShowThree = `${iMrdo}<br>
+                       ${iMrdoQ}<br>
+                       ${iMrdoQP}<br>`;
+
+let textToShow = `${textToShowOne} 
+                  <br> 
+                  ${textToShowTwo}
+                  <br>
+                  ${textToShowThree}`;
 
 
+//Imprimir
 
+document.querySelector(".rNumDos").innerHTML = "Respuesta";
+
+let mainAnswer = document.querySelector('.rDos');
+mainAnswer.innerHTML = `${textToShow}`;
 
 
 
